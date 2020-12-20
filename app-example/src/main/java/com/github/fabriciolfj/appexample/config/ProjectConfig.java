2a142c80a28e0e241d42c99282e3f60b186928c8
Fabricio <fabricio.jacob@outlook.com>
2020-12-20 18:12:01 -0300

    add

--- app-example/src/main/java/com/github/fabriciolfj/appexample/config/ProjectConfig.java
+++ app-example/src/main/java/com/github/fabriciolfj/appexample/config/ProjectConfig.java
@@ -3,11 +3,16 @@ package com.github.fabriciolfj.appexample.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class ProjectConfig {
@@ -19,6 +24,12 @@ public class ProjectConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+        /*Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder());
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);*/
         return NoOpPasswordEncoder.getInstance();
     }
 
--- app-example/src/main/java/com/github/fabriciolfj/appexample/controller/HelloWorldController.java
+++ app-example/src/main/java/com/github/fabriciolfj/appexample/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.github.fabriciolfj.appexample.controller;
 
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.security.crypto.encrypt.TextEncryptor;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +13,12 @@ public class HelloWorldController {
 
     @GetMapping
     public String hello() {
-        return "Hello!";
+        String salt = KeyGenerators.string().generateKey();
+        String password = "secret";
+        String valueToEncrypt = "HELLO";
+        TextEncryptor e = Encryptors.queryableText(password, salt);
+        String encrypted1 = e.encrypt(valueToEncrypt);
+
+        return encrypted1;
     }
 }
