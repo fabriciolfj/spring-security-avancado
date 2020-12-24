cd4319a8f4b546b06e053203ca495eb9d2948416
fabricio <fabricio.jacob@outlook.com>
2020-12-24 07:52:23 -0300

    add

---
+++ app-exemplo1/src/main/java/com/github/fabriciolfj/appexemplo1/AppExemplo1Application.java
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.appexemplo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppExemplo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppExemplo1Application.class, args);
+	}
+
+}
---
+++ app-exemplo1/src/test/java/com/github/fabriciolfj/appexemplo1/AppExemplo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fabriciolfj.appexemplo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppExemplo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
